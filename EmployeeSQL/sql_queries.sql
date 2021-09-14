e6251d127db9f3f66d84da9bfadfebe5a9021313
sgkuzmin <87283907+sgkuzmin@users.noreply.github.com>
2021-09-14 18:05:13 -0500

    main part of homework

--- EmployeeSQL/sql_queries.sql
+++ EmployeeSQL/sql_queries.sql
@@ -16,6 +16,44 @@ from (departments dp
 left join dept_manager dm on (dp.dept_no = dm.dept_no)
 	 left join employees em on (dm.emp_no=em.emp_no))
 	 order by dept_no;
+	 
+--- 4 List the department of each employee with the following information: 
+---        employee number, last name, first name, and department name.
 
+select em.emp_no, em.last_name, em.first_name, dp.dept_name
+from (employees em 
+left join dept_emp de on (em.emp_no = de.emp_no)) 
+left join departments dp on (de.dept_no= dp.dept_no) 
+order by emp_no;
 
+--- 5 List first name, last name, and sex for employees whose 
+---      first name is "Hercules" and last names begin with "B."
 
+select first_name, last_name, sex from employees 
+where (first_name='Hercules' and (SELECT LEFT(last_name, 1))='B');
+
+--- 6 List all employees in the Sales department, including their 
+---          employee number, last name, first name, and department name.
+
+select em.emp_no, em.last_name, em.first_name, dp.dept_name
+from ((employees em 
+	 left join dept_emp de on (em.emp_no = de.emp_no))
+	 left join departments dp on (de.dept_no=dp.dept_no)) 
+	 where dept_name='Sales';
+	 
+--- 7 List all employees in the Sales and Development departments, 
+---    including their employee number, last name, first name, and department name.
+
+select em.emp_no, em.last_name, em.first_name, dp.dept_name
+from ((employees em 
+	 left join dept_emp de on (em.emp_no = de.emp_no))
+	 left join departments dp on (de.dept_no=dp.dept_no)) 
+	 where ( dept_name='Sales' or dept_name='Development');
+	 
+--- 8 In descending order, list the frequency count of employee last names, 
+---         i.e., how many employees share each last name.
+
+select last_name, count(last_name) as "Last name count"
+from employees
+group by last_name
+order by "Last name count" desc; 
